e360da5b5edaf2ee0f577a5289c8bd4b60c703d5
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-01-08 10:45:33 +0530

    Time: 1393 ms (5.05%), Space: 201.2 MB (5.35%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,18 +1,30 @@
 class Solution {
+private:
+    int solve(int currentIdx,int jobs,vector<vector<int>>& events,vector<vector<int>>& dp){
+        //base condition
+        if(currentIdx==events.size() || jobs==2)
+            return 0;
+        
+        if(dp[currentIdx][jobs]!=-1)
+            return dp[currentIdx][jobs];
+        
+        vector<int>vec={events[currentIdx][1],INT_MAX,INT_MAX};
+        int nextIdx=upper_bound(events.begin(),events.end(),vec)-events.begin();
+        
+        //can perform
+        int val1=events[currentIdx][2]+solve(nextIdx,jobs+1,events,dp);
+        //cannot perform
+        int val2=solve(currentIdx+1,jobs,events,dp);
+        
+        return dp[currentIdx][jobs]=max(val1,val2);
+    }
+    
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
         int n=events.size();
-        priority_queue<pair<int,pair<int,int>>>pq;
+        vector<vector<int>>dp(n,vector<int>(2,-1));
         sort(begin(events),end(events));
-        int ans=0,maxValue=0;
-        for(int i=0;i<n;i++){
-            while(!pq.empty() and (-1*pq.top().first)<events[i][0]){
-                maxValue=max(maxValue,pq.top().second.second);
-                pq.pop();
-            }
-            ans=max(ans,maxValue+events[i][2]);
-            pq.push({-1*events[i][1],{events[i][0],events[i][2]}});
-        }
-        return ans;
+        
+        return solve(0,0,events,dp);
     }
 };
