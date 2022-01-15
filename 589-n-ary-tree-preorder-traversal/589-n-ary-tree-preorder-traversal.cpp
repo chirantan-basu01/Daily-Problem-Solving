9bcd7c26648ed19647ccbf66113b839697cbb53a
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-01-15 22:09:13 +0530

    Time: 48 ms (10.34%), Space: 11 MB (98.89%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -20,21 +20,25 @@ public:
 
 class Solution {
 public:
-    
-    void preorderTrav(Node* root,vector<int>&ans){
-        if(root==NULL)
-            return;
-        
-        ans.push_back(root->val);
-        for(Node *child:root->children){
-            preorderTrav(child,ans);
-        }
-        return;
-    }
-    
     vector<int> preorder(Node* root) {
-        vector<int>ans;
-        preorderTrav(root,ans);
-        return ans;
+    vector<int> ans;
+    if(root) {
+        stack<Node*> st;
+        st.push(root);
+
+        while(!st.empty()) {
+            ans.push_back(st.top() -> val);
+            Node *temp = st.top();
+            st.pop();
+            if((temp -> children).size()) {
+                auto it = (temp -> children).rbegin();
+                while (it != (temp -> children).rend()) {
+                    st.push(*it);
+                    it++;
+                };
+            }
+        }
+    }
+    return ans;
     }
 };
