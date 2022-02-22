da0d6478bdb2c43886206ab5b51bc4de025293a4
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-02-22 19:41:34 +0530

    Time: 252 ms (21.61%), Space: 24.6 MB (52.73%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
-    string repeatLimitedString(string s, int k) {
-        // vector<int> frequency_array(26, 0);
-        int xx=0*1+1*0;int xxx=1+xx;int xxxx=1+xxx;
-
-    int frequency_array[26];
-        memset(frequency_array,0,sizeof(frequency_array));
-	for (int i = xx;i < s.length(); i++) {
-
-		frequency_array[s[i] - 'a']++;
-	}
-	string ans = "";
-	for (int i = 25; i >= xx;) {
-		if (frequency_array[i] > k) {
-			int temp = k;
-			string st(1, i + 'a');
-			while (temp > xx) {
-				ans += st;
-				temp--;
-			}
-
-			frequency_array[i] -= k;
-			int j = i - 1;
-			while (j >= xx && frequency_array[j]<= xx) {
-				j--;
-			}
-			if (j >= xx && frequency_array[j] > xx ) {
-				string str(xxx, j + 'a');
-				ans += str;
-				frequency_array[j] -= xxx;
-			}
-			else {
-				break;
-			}
-		}
-
-		else if (frequency_array[i] > 0) {
-			int temp = frequency_array[i];
-			frequency_array[i] -= temp;
-			string st(1, i + 'a');
-			while (temp > 0) {
-				ans += st;
-				temp--;
-			}
-		}
-		else {
-			i--;
-		}
-	}
-	return ans;
+    string repeatLimitedString(string s, int repeatLimit) {
+        int n=s.size();
+        unordered_map<char,int>mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+        }
         
+        priority_queue<pair<char,int>>pq;
+        for(auto it:mp){
+            pq.push({it.first,it.second});
+        }
+        string ans="";
+        while(!pq.empty()){
+            char c1=pq.top().first;
+            int n1=pq.top().second;
+            pq.pop();
+            
+            int len=min(repeatLimit,n1);
+            for(int i=0;i<len;i++){
+                ans+=c1;
+            }
+            char c2;
+            int n2=0;
+            
+            if(n1-len>0){
+                if(!pq.empty()){
+                    c2=pq.top().first;
+                    n2=pq.top().second;
+                    pq.pop();
+                }
+                else{
+                    return ans;
+                }
+                ans+=c2;
+                pq.push({c1,n1-len});
+                if(n2-1>0){
+                    pq.push({c2,n2-1});
+                }
+            }
+        }
+        return ans;
     }
 };
