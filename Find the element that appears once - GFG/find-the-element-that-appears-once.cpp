18d6d93e1120463d344edf1d401e20548a484088
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-03-07 20:11:15 +0530

    Added solution - LeetHub

---
+++ Find the element that appears once - GFG/find-the-element-that-appears-once.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int search(int A[], int N){
+	    //code
+	    unordered_map<int,int>mp;
+	    for(int i=0;i<N;i++){
+	        mp[A[i]]++;
+	    }
+	    
+	    for(int i=0;i<N;i++){
+	        if(mp[A[i]]==1){
+	            return A[i];
+	        }
+	    }
+	    return -1;
+	}
+};
+
+// { Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t,len;
+    cin>>t;
+    while(t--)
+    {
+        cin>>len;
+        int arr[len];
+        for(int i=0;i<len;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.search(arr, len)<<'\n';
+    }
+    return 0;
+}
+  // } Driver Code Ends
