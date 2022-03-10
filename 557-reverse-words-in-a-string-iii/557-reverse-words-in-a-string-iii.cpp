552711ff3063f844d134b9f185f004db5013ecad
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-03-10 20:10:20 +0530

    Time: 18 ms (59.51%), Space: 11.4 MB (27.01%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string result,word;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' ')
+                word+=s[i];
+            else{
+                reverse(word.begin(),word.end());
+                result+=word;
+                result+=' ';
+                word.clear();
+            }
+        }
+        reverse(word.begin(),word.end());
+        result+=word;
+        return result;
+    }
+};
