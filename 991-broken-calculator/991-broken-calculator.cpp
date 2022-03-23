46891da132b1b485a86c147e3527cefd0d854519
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-03-23 09:58:03 +0530

    Time: 2 ms (37.76%), Space: 6 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count = 0;
+        
+        while(target > startValue) {
+            if(target % 2 != 0) {
+                target++;
+            }
+            else {
+                target = target / 2;
+            }
+            count++;
+        }
+        return count + startValue - target;
+    }
+};
