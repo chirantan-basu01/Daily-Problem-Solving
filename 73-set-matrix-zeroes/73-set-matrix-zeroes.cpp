df0f7736469a1535f26dc84c037e9033a56790b6
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-05-10 10:15:28 +0530

    Time: 24 ms (28.28%), Space: 13.8 MB (11.54%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        map<int,int>row,col;
+        map<int,string>row,col;
         
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    row[i]="#";
+                    col[j]="#";
                 }
             }
         }
         
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                if(row[i]==1 || col[j]==1){
+                if(row[i]=="#" || col[j]=="#"){
                     matrix[i][j]=0;
                 }
             }
         }
-        
     }
 };
