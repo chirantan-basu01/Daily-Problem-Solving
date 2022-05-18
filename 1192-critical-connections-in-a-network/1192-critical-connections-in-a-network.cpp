8286148cec684b55f819ba148a9c86110ef5530e
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-05-18 10:28:19 +0530

    Time: 1016 ms (34.47%), Space: 193.6 MB (25.95%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>>ans,graph;
+    vector<int>lowlink,id;
+    vector<bool>vis;
+    int time=0;
+    
+    void DFS(int node,int parent){
+        vis[node]=true;
+        id[node]=lowlink[node]=time++;
+        
+        for(auto it:graph[node]){
+            if(parent==it)
+                continue;
+            if(vis[it]==false){
+                DFS(it,node);
+                lowlink[node]=min(lowlink[node],lowlink[it]);
+                if(id[node]<lowlink[it])
+                    ans.push_back({node,it});
+            }
+            else{
+                lowlink[node]=min(lowlink[node],id[it]);
+            }
+        }
+    }
+    
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>&connections) {
+        graph.resize(n);
+        vis.resize(n,false);
+        lowlink.resize(n,0);
+        id.resize(n,0);
+        for(auto it: connections){
+            graph[it[0]].push_back(it[1]);
+            graph[it[1]].push_back(it[0]);
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                DFS(i,i);
+            }
+        }
+        return ans;
+    }
+};
