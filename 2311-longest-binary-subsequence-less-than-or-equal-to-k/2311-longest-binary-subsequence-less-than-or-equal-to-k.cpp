819183af8385fdfbec36415c9f0407b56986c56f
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-06-19 19:03:16 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (100.00%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-       long long z=0,o=0,n=s.length();
-        vector<long long> zeros,ones;
-        for(long long i=n-1;i>=0;i--){
-            char c=s[i];
-            z+=c=='0';
-            o+=c=='1';
-            zeros.push_back(z);
-            ones.push_back(o);
-        }
-        reverse(s.begin(),s.end());
-        long long ans=z;
-        long long val=0;
-        for(long long i=0;i<n && k>0;i++){
-            if(s[i]=='1'){
-                if(i>31)return ans;
-                long long p=(long long)pow(2,i);
-                if(p<=k){
-                    k-=p;
+        int j=0,ans=0,sum=0;
+        bool stop=false;
+        
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='0')
+                ans++;
+            else if(stop==0 and j<31){
+                sum+=pow(2,j);
+                if(sum>k){
+                    stop=1;
+                    sum-=pow(2,j);
+                }
+                else{
                     ans++;
                 }
-                else return ans;
             }
+            j++;
         }
-        return ans; 
+        return ans;
     }
 };
